e8ec564498ffb42889a85ecb6ef2d5266a08ccb5
Rodrigo <71734355+youtubeitsnice@users.noreply.github.com>
2020-11-23 19:43:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,15 +11,16 @@ function addElement(){
         var pPalavra=inputl.value.toUpperCase().split("");
         var rPalavra=inputl.value.toLowerCase().slice(1,20);
         var li=document.createElement("li");
-        var liTxt=document.createTextNode(pPalavra[0]+rPalavra);
+        li.textContent=pPalavra[0]+rPalavra;
 
-        li.appendChild(liTxt);
         lista.appendChild(li);
         inputl.value="";
         inputl.focus();
         inputl.disabled="";
         inputcont.innerHTML="20";
         inputcont.style.color="#23b6fa";
+        selOg.disabled="";
+        selOg.style.cursor="pointer";
         
         listaF();
     }else{
@@ -89,8 +90,9 @@ btnDel.addEventListener("click",function(){
     if(res==0){
         alert("Não há elementos na lista!");
     }else{
+        lista.remove();
+
         for(let i=0;i<res;i++){
-            lista.childNodes[i+1].remove();
             res-=res;
             ulLista.innerHTML=`Lista: (${res})`;
         }
@@ -99,7 +101,7 @@ btnDel.addEventListener("click",function(){
     }
 });
 
-selOg.addEventListener("click",function(){
+selOg.addEventListener("change",function(){
     if(selOg.value=="alphabetic-o"){
         let mochila=[];
         let res=lista.childElementCount;
@@ -108,14 +110,19 @@ selOg.addEventListener("click",function(){
         for(let i=0;i<res+1;i++){
             mochila.push(lista.childNodes[i].innerHTML);
             mochila.sort();
-
             novoLi.textContent=mochila[i];
             lista.appendChild(novoLi);
         }
 
+        mochila.pop();
         console.log(mochila);
     }
 });
 
 inputl.addEventListener("keypress",continput);
 btnAdd.addEventListener("click",addElement);
+
+if(lista.childElementCount==0){
+    selOg.disabled="true";
+    selOg.style.cursor="default";
+}
